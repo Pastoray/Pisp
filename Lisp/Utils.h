54436bc8114a2816799d73830bf0ea4de340d9b7
Pastoray <pastoriaraar69@gmail.com>
2025-03-14 18:09:03 +0100

    remove redundent err_exit defs and implement a better logger

--- Lisp/Utils.h
+++ Lisp/Utils.h
@@ -2,7 +2,20 @@
 #include <iostream>
 
 #ifdef _DEBUG
-#define LOG_DEBUG(msg) std::cout << msg
+#define logger std::cout
 #else
-#define LOG_DEBUG(msg) ((void)0)
+
+struct NullStream
+{
+	template<typename T>
+	NullStream& operator<<(const T&) { return *this; }
+} null_logger;
+
+#define logger null_logger
 #endif
+
+inline void err_exit(const std::string & context, const size_t index, const std::string & msg)
+{
+	std::cerr << "[ERROR]" << "[CONTEXT:" << context << "]" << "[INDEX:" << index << "]" << " -> " << msg << std::endl;
+	exit(EXIT_FAILURE);
+}
